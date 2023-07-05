353a540a32261accdf366634dd8a42515ca2824c
Vidar Rehn <vidar.rehn@gmail.com>
2023-07-05 13:21:35 +0200

    added GeinsKlaviyoForm component

---
+++ lib/components/atoms/GeinsKlavioForm/GeinsKlaviyoForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="geins-klaviyo-form">
+        GeinsKlaviyoForm
+    </div>
+</template>
+<script>
+/* 
+  Streamify Live Shopping Player
+*/
+
+export default {
+    name: 'GeinsKlaviyoForm',
+    mixins: [MixAddToCart],
+    props: {},
+    data: () => ({}),
+    computed: {},
+    watch: {},
+    async mounted() { },
+    methods: {}
+};
+</script>
+  
---
+++ lib/components/molecules/GeinsWidgetKlaviyoForm.js/GeinsWidgetKlaviyoForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="geins-widget-klaviyo-form">
+        <GeinsWidgetKlaviyoForm />
+    </div>
+</template>
+<script>
+/* 
+  Streamify Live Shopping Player
+*/
+
+export default {
+    name: 'GeinsWidgetKlaviyoForm',
+    mixins: [MixAddToCart],
+    props: {},
+    data: () => ({}),
+    computed: {},
+    watch: {},
+    async mounted() { },
+    methods: {}
+};
+</script>
+  
